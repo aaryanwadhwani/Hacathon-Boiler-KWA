8a6e487c2473fdeba68264184c881cd3ed62d48d
aryamansh30 <sharm782@purdue.edu>
2025-02-22 03:01:59 -0500

    removed everything

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-        <div>
-            <h1>ATS Resume Reviewer</h1>
-            <p>Upload your resume to get ATS insights.</p>
-        </div>
-    );
-}
-export default App;
