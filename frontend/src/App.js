61e1e538bc6cc1ae3c39eb08ea28935ea80846cf
aryamansh30 <sharm782@purdue.edu>
2025-02-22 02:24:41 -0500

    Initialized React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <h1>ATS Resume Reviewer</h1>
+            <p>Upload your resume to get ATS insights.</p>
+        </div>
+    );
+}
+export default App;
