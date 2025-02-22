cc6b1a624b1a50260276f76ee53217346124cccc
Aaryan Wadhwani <wadhwaniaaryan@gmail.com>
2025-02-22 03:47:29 -0500

    initializing

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
